29d7be8b1ff2816ea1527680719384a54e09349f
prashant679 <prashantmail1808@gmail.com>
2026-07-23 19:11:44 +0530

    Added api key and connected it to OpenAi

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+#loading open router API key
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+if api_key == None:
+    raise RuntimeError("openrouter api key is not connected! \nCheck your env")
+
+#making a clien using openai
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+message=[
+    {
+        "role": "user",
+        "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    }
+]
+
+response = client.chat.completions.create(
+    model = "openrouter/free",
+    messages=message
+    )
+if response.usage == None:
+    raise RuntimeError("Failed API request")
+
+print(f"Prompt tokens: {response.usage.prompt_tokens}\nResponse tokens: {response.usage.completion_tokens}")
+print(response.choices[0].message.content)
+
 def main():
     print("Hello from ai-clibot!")
 
